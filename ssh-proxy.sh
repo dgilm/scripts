32e741055b06d98dee34f635fc0559f1b1a13073
David Gil <dgil@a3sec.com>
2015-01-26 09:08:01 +0100

    a few system scripts, more coming

---
+++ ssh-proxy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PROXY_IP="172.24.8.161"
+PROXY_PORT="3128"
+
+CORKSCREW=`which corkscrew`
+if [ x$CORKSCREW == "x" ]; then
+	echo "ERROR: Corkscrew is not installed."
+	echo "corkscrew is a tool to tunnel TCP connections through an HTTP proxy"
+	exit 1
+fi
+
+function usage {
+	echo "Usage: $0 -r remote-server [-u user] [-p port]"
+	exit 1
+}
+
+while getopts "u:p:r:" o; do
+    case "${o}" in
+    	r)	REMOTE_SERVER=${OPTARG} ;;
+        u)	REMOTE_USER=${OPTARG} ;;
+        p)	REMOTE_PORT=${OPTARG} ;;
+        *)	usage ;;
+    esac
+done
+
+[ -z $REMOTE_SERVER ] && usage
+[ -z $REMOTE_USER ] && REMOTE_USER=`whoami`
+[ -z $REMOTE_PORT ] && REMOTE_PORT=22
+
+echo "Connecting to $REMOTE_USER@$REMOTE_SERVER:$REMOTE_PORT using proxy $PROXY_IP:$PROXY_PORT..."
+
+ssh $REMOTE_USER@$REMOTE_SERVER -o \
+	"ProxyCommand $CORKSCREW $PROXY_IP $PROXY_PORT $REMOTE_SERVER $REMOTE_PORT"
